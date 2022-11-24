50408653384f611325977364862b4877a34d56e3
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2022-11-24 11:48:30 +0100

    docs: fonction retourne Le plus grand en 3 parametres

---
+++ exerciceQuatre.ts
@@ -0,0 +1,9 @@
+/**
+ * Fonction qui retourne le plus nombre des paramétres entrés dans la fonction
+ */
+
+function laLoiDuPlusGrand(un:number, deux:number, trois:number) {
+    return Math.max(un, deux, trois);
+}
+
+console.log(laLoiDuPlusGrand(4, 12, 9));
