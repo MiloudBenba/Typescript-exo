48ecfc81f5c9f4adaeb13a52b398249c2d3360cf
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2022-11-24 11:23:57 +0100

    Docs: exercice 1

---
+++ helloWorld.ts
@@ -0,0 +1,12 @@
+
+/**
+ * function Prenom pour retourner Bonjour + prénom en paramétre
+ */
+
+
+function prenom(name: string) :string {
+return `Bonjour ${name}`;
+}
+
+console.log(prenom("Joe"));
+
