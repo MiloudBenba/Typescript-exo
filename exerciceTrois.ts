2b94357ea878f9ab6bc2470886f9bd279f677f8d
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2022-11-24 11:40:14 +0100

    docs: fonction Benefice 2 parametres

---
+++ exerciceTrois.ts
@@ -0,0 +1,16 @@
+/**
+ * Fonction qui prend deux paramétres 
+ * (prix de vente + cout de fabrication), et retourne la différence (bénéfice, perte).
+ */
+
+function benefice(coutFab:number, prixVente:number) {
+    if(coutFab > prixVente){
+        return `Perte de ${coutFab - prixVente}`;
+    } else if(coutFab < prixVente){
+        return `Profit de ${prixVente - coutFab}`;
+    } else {
+        return `Ni profit, ni perte. Vive le benevolat !`;
+    }
+}
+
+console.log(benefice(10, 15));
